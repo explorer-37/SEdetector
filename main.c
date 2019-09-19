cf5661d0beef4d2953997cfcce429fff0f59befa
U-DESKTOP-DPE6296\nakamura <explorer.37@s.okayama-u.ac.jp>
2019-09-19 18:02:33 +0900

    add main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "SEdetector.h"
+
+int main(int argc, char *argv[]){
+	// initialize
+	
+	// create process of malware
+	
+	// start monitoring WinAPI (using dll injection)
+	
+	// detect sandbox evasion
+	
+}
