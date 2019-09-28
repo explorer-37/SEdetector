cfd7a9461b26052251add4282eb1af9845aa4cfe
U-DESKTOP-DPE6296\nakamura <explorer.37@s.okayama-u.ac.jp>
2019-09-28 18:47:04 +0900

    modify apimonitor

--- apimonitor.c
+++ apimonitor.c
@@ -1,34 +1,72 @@
 #include "apimonitor.h"
 
 int WINAPI DllMain(HINSTANCE hInstance, DWORD fdwReason, PVOID pvReserved){
-	// get WinAPIs
-	HMODULE kernel32;
-	HMODULE shell32;
-	if((kernel32 = GetModuleHandle("kernel32"))) {
-		oriIsDebuggerPresent = GetProcAddress(kernel32, "IsDebuggerPresent");
-	}
-	if((shell32 = GetModuleHandle("shell32"))) {
-		oriShellAboutW = GetProcAddress(shell32, "ShellAboutW");
-	}
+	//HMODULE kernel32;
+	//if((kernel32 = GetModuleHandle("kernel32"))) {
+	//	oriapi[IDX_ISDEBUGGERPRESENT] = GetProcAddress(kernel32, "IsDebuggerPresent");
+	//}
 
 	switch (fdwReason) {
 		case DLL_PROCESS_ATTACH:
+			// get WinAPIs
+			GetApiEntry();
 			// set API hook
-			ModifyIat("kernel32.dll", newIsDebuggerPresent, oriIsDebuggerPresent);
-			ModifyIat("shell32.dll", newShellAboutW, oriShellAboutW);
+			ModifyIat("kernel32.dll", newIsDebuggerPresent, oriapi[IDX_ISDEBUGGERPRESENT].addr);
+			ModifyIat("kernel32.dll", newGetFileAttributes, oriapi[IDX_GETFILEATTRIBUTES].addr);
+			ModifyIat("Advapi32.dll", newRegOpenKeyEx, oriapi[IDX_REGOPENKEYEX].addr);
 			//CreateNamedPipe("\\\\.\\pipe\\SEdetector",);
 			IsDebuggerPresent(); // for debugging
 			break;
 		case DLL_PROCESS_DETACH:
 			// restore IAT
-			ModifyIat("kernel32.dll", oriIsDebuggerPresent, newIsDebuggerPresent);
-			ModifyIat("shell32.dll", oriShellAboutW, newShellAboutW);
+			IsDebuggerPresent(); // for debugging
+			ModifyIat("kernel32.dll", oriapi[IDX_ISDEBUGGERPRESENT].addr, newIsDebuggerPresent);
+			ModifyIat("kernel32.dll", oriapi[IDX_GETFILEATTRIBUTES].addr, newGetFileAttributes);
+			ModifyIat("Advapi32.dll", oriapi[IDX_REGOPENKEYEX].addr, newRegOpenKeyEx);
 			break;
 		default:
 			;
 	}
 
 	return 1;
+}
+
+void GetApiEntry(){
+	HMODULE kernel32;
+	HMODULE advapi32;
+	if((kernel32 = GetModuleHandle("kernel32"))) {
+		{
+			char *name = "IsDebbugerPresent";
+			oriapi[IDX_ISDEBUGGERPRESENT].addr = GetProcAddress(kernel32, "IsDebuggerPresent");
+			oriapi[IDX_ISDEBUGGERPRESENT].Name = name;
+			oriapi[IDX_ISDEBUGGERPRESENT].NumArg = 0;
+		}
+		{
+			char *name = "GetFileAttributes";
+			oriapi[IDX_GETFILEATTRIBUTES].addr = GetProcAddress(kernel32, "GetFileAttributes");
+			oriapi[IDX_GETFILEATTRIBUTES].Name = name;
+			oriapi[IDX_GETFILEATTRIBUTES].NumArg = 1;
+			oriapi[IDX_GETFILEATTRIBUTES].Arg = (int *)malloc(sizeof(int));
+			oriapi[IDX_GETFILEATTRIBUTES].Arg[0] = TYPE_STRING;
+		}
+	}
+	if((advapi32 = GetModuleHandle("Advapi32"))) {
+		{
+			char *name = "RegOpenKeyEx";
+			oriapi[IDX_REGOPENKEYEX].addr = GetProcAddress(advapi32, "RegOpenKeyEx");
+			oriapi[IDX_REGOPENKEYEX].Name = name;
+			oriapi[IDX_REGOPENKEYEX].NumArg = 5;
+			oriapi[IDX_REGOPENKEYEX].Arg = (int *)malloc(sizeof(int) * 5);
+			oriapi[IDX_REGOPENKEYEX].Arg[0] = TYPE_ADDR;
+			oriapi[IDX_REGOPENKEYEX].Arg[1] = TYPE_STRING;
+			oriapi[IDX_REGOPENKEYEX].Arg[2] = TYPE_INT;
+			oriapi[IDX_REGOPENKEYEX].Arg[3] = TYPE_ATTRIBUTE16;
+			oriapi[IDX_REGOPENKEYEX].Arg[4] = TYPE_ADDR;
+		}
+	}
+
+	CloseHandle(kernel32);
+	CloseHandle(advapi32);
 }
 
 void ModifyIat(char *dllname, void *newaddr, void *oldaddr){
@@ -89,20 +127,63 @@ void ModifyIatOne(char *dllname, void *newaddr, void *oldaddr, HMODULE hModule){
 	}
 }
 
-void GetCallApi(char *apiname, ...){
+void GetCallApi(int ApiIndex, ...){
 	// send API information using named pipe
-	char str[512] = {};
-	sprintf(str, "%256s called.", apiname); // for debugging
+	int i;
+	char str[MAX_STR], tmp[MAX_STR];
+	va_list arglist;
+	int arg_int; unsigned short arg_uint16; unsigned int arg_uint32; u_int64 arg_uint64; char *arg_str;
+
+	strncpy(str, oriapi[ApiIndex].Name, MAX_STR);
+
+	if (oriapi[ApiIndex].NumArg > 0) {
+		va_start(arglist, ApiIndex);
+		for (i = 0; i < oriapi[ApiIndex].NumArg; i++) {
+			switch (oriapi[ApiIndex].Arg[i]) {
+				case TYPE_INT:
+					arg_int = (int)va_arg(arglist, int);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %d", tmp, arg_int);
+				case TYPE_ADDR:
+					arg_uint64 = (u_int64)va_arg(arglist, u_int64);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %I64x", tmp, arg_uint64);
+				case TYPE_ATTRIBUTE16:
+					arg_uint16 = (unsigned short)va_arg(arglist, unsigned int);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %x", tmp, arg_uint16);
+				case TYPE_ATTRIBUTE32:
+					arg_uint32 = (unsigned int)va_arg(arglist, unsigned int);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %x", tmp, arg_uint32);
+				case TYPE_ATTRIBUTE64:
+					arg_uint64 = (u_int64)va_arg(arglist, u_int64);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %I64x", tmp, arg_uint64);
+				case TYPE_STRING:
+					arg_str = (char *)va_arg(arglist, char*);
+					strncpy(tmp, str, MAX_STR);
+					snprintf(str, MAX_STR, "%s, %s", tmp, arg_str);
+				default:
+					;
+			}
+		}
+		va_end(arglist);
+	}
 	MessageBox(NULL, str, "debug", MB_SYSTEMMODAL); // for debugging
 	return;
 }
 
 BOOL WINAPI newIsDebuggerPresent(){
-	GetCallApi("IsDebuggerPresent");
-	return oriIsDebuggerPresent();
+	GetCallApi(IDX_ISDEBUGGERPRESENT);
+	return oriapi[IDX_ISDEBUGGERPRESENT].addr();
 }
 
-INT WINAPI newShellAboutW(HWND hWnd, LPCWSTR szApp, LPCWSTR szOtherStuff, HICON hIcon){
-	GetCallApi("ShellAboutW", hWnd, szApp, szOtherStuff, hIcon);
-	return oriShellAboutW(hWnd, L"ShellAboutW hooked!", szOtherStuff, hIcon); // for debugging
+DWORD WINAPI newGetFileAttributes(LPCTSTR lpFileName){
+	GetCallApi(IDX_GETFILEATTRIBUTES, lpFileName);
+	return oriapi[IDX_GETFILEATTRIBUTES].addr();
+}
+LONG newRegOpenKeyEx(HKEY hKey, LPCTSTR lpSubKey, DWORD ulOptions, REGSAM samDesired, PHKEY phkResult){
+	GetCallApi(IDX_REGOPENKEYEX, hKey, lpSubKey, ulOptions, samDesired, phkResult);
+	return oriapi[IDX_REGOPENKEYEX].addr();
 }
--- apimonitor.h
+++ apimonitor.h
@@ -2,15 +2,40 @@
 #include <windows.h>
 #include <tlhelp32.h>
 
+#define MAX_ORIAPI 256
+
+#define MAX_STR 65536
+
+#define IDX_ISDEBUGGERPRESENT 0
+#define IDX_GETFILEATTRIBUTES 1
+#define IDX_REGOPENKEYEX 2
+
+#define TYPE_INT 0
+#define TYPE_ADDR 1
+#define TYPE_ATTRIBUTE16 2
+#define TYPE_ATTRIBUTE32 3
+#define TYPE_ATTRIBUTE64 4
+#define TYPE_STRING 5
+
+typedef struct _APIINFO {
+	FARPROC addr;
+	char *Name;
+	int NumArg;
+	int *Arg;
+} APIINFO;
+
+void GetApiEntry();
+
 void ModifyIat(char *dllname, void *newaddr, void *oldaddr);
 PIMAGE_IMPORT_DESCRIPTOR GetImportEntry(PVOID Base, PULONG Size);
 void ModifyIatOne(char *dllname, void *newaddr, void *oldaddr, HMODULE hModule);
 
-void GetCallApi(char *apiname, ...);
+void GetCallApi(int ApiIndex, ...);
 
 
 // proclaim of hook api
 BOOL newIsDebuggerPresent();
-FARPROC oriIsDebuggerPresent;
-INT newShellAboutW(HWND hWnd, LPCWSTR szApp, LPCWSTR szOtherStuff, HICON hIcon);
-FARPROC oriShellAboutW;
+DWORD newGetFileAttributes(LPCTSTR lpFileName);
+LONG newRegOpenKeyEx(HKEY hKey, LPCTSTR lpSubKey, DWORD ulOptions, REGSAM samDesired, PHKEY phkResult);
+
+APIINFO oriapi[MAX_ORIAPI];
