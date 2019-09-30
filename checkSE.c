a88a4ac82eda06af7301142ad8f34ac45ad3d810
U-DESKTOP-DPE6296\nakamura <explorer.37@s.okayama-u.ac.jp>
2019-09-30 19:35:34 +0900

    append CheckVmwareRegKey()

--- checkSE.c
+++ checkSE.c
@@ -18,11 +18,31 @@ int CheckSE(APIINFO *info, int num_info){
 	return SE_NONE;
 }
 
+int CheckVmwareRegKey(APIINFO *info, int num_info){
+	int i;
+	for (i = 0; i < num_info; i++) {
+		if (info[i]->Index == IDX_REGOPENKEYEXA) {
+			unsigned long long *hKey = (unsigned long long *)info[i]->Arg[0];
+			char *SubKey = (char *)info[i]->Arg[1];
+			if (*hKey == HKEY_LOCAL_MACHINE) {
+				if (
+						strncmp(SubKey, "SOFTWARE\\VMware, Inc.\\VMware Tools", MAX_BUF) == 0 ||
+						strncmp(SubKey, "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 0\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", MAX_BUF) == 0 ||
+						strncmp(SubKey, "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 1\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", MAX_BUF) == 0 ||
+						strncmp(SubKey, "HARDWARE\\DEVICEMAP\\Scsi\\Scsi Port 2\\Scsi Bus 0\\Target Id 0\\Logical Unit Id 0", MAX_BUF) == 0
+				)
+					return 1;
+			}
+		}
+	}
+	return 0;
+}
+
 // an example function which detect one of SE methods
 int SEExample(APIINFO *info, int num_info){
 	int i;
 	for (i = 0; i < num_info; i++) {
-		if (info[i].Index == IDX_INVALID_API) {
+		if (info[i]->Index == IDX_INVALID_API) {
 			return 1;
 		}
 	}
